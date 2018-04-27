4608e2b222d0f9a27432ac3e52ff6e35e0a8579f
lhl <1079161148@qq.com>
2018-04-27 23:22:15 +0800

    提交element ui项目骨架(sass语法)

---
+++ element_demo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ele-header></ele-header>
+    <router-view/>
+    <ele-footer></ele-footer>
+  </div>
+</template>
+
+<script>
+import eleHeader from './components/eleHeader'
+import eleFooter from './components/eleFooter'
+export default {
+  name: 'App',
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    eleHeader,
+    eleFooter,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ element_demo/src/pages/carousel.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="wrap_content">
+        <p>xxxx跑马灯</p>
+    </div>
+</template>
+
+<script type="text/javascript">
+export default{
+    data(){
+        return{
+
+        }
+    },
+    created(){
+
+    },
+}
+</script>
+
+<style lang="scsss" scoped>
+
+</style>
